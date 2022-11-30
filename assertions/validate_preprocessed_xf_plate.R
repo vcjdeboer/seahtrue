4d62ed1c78fe0e03ee29cf24f404906f874eb54e
Gerwin Smits <smitsg@ziggo.nl>
2022-11-30 14:58:46 +0000

    feat: Add assertions to check preprocessed seahorse plate data.

---
+++ assertions/validate_preprocessed_xf_plate.R
@@ -0,0 +1,495 @@
+# Create a list of data quality demands with the validator() function.
+# Rules for prepossessed seahorse data, "Raw" tibble.
+xf_plate_pr_raw_rules <- validator(
+    # Rules for well
+    well_character = is.character(well),
+    ### well_field_length = field_length(well, n=3),
+    well_miss_values = all_complete(well),
+    well_number = all(grepl("^[A-H](0[1-9]|1[0-1])\b$|$", well)),
+
+    # Rules for measurement
+    measurement_integer = is.integer(measurement),
+    measurement_min_1 = all(measurement >= 1),
+    measurement_miss_values = all_complete(measurement),
+
+    # Rules for tick
+    tick_numeric = is.numeric(tick),
+    tick_min_0 = all(tick >= 0),
+    tick_miss_values = all_complete(tick),
+
+    # Rules for timescale
+    timescale_numeric = is.numeric(timescale),
+    timescale_miss_values = all_complete(timescale),
+    timescale_min_0 = all(timescale >= 0),
+
+    # Rules for minutes
+    minutes_numeric = is.numeric(minutes),
+    # grepl("^[0-9]*.[0-9]*$", minutes)
+    minutes_miss_values = all_complete(minutes),
+
+    # Rules for group
+    group_character = is.character(group),
+    group_miss_values = all_complete(group),
+
+    # # Rules for interval
+    interval_numeric = is.numeric(interval),
+    interval_miss_values = all_complete(interval),
+
+    # # Rules for injection
+    injection_character = is.character(injection),
+    injection_miss_values = all_complete(injection),
+
+    # Rules for O2_em_corr
+    O2_em_corr_numeric = is.numeric(O2_em_corr),
+    O2_em_corr_miss_values = all_complete(O2_em_corr),
+
+    # Rules for pH_em_corr
+    pH_em_corr_numeric = is.numeric(pH_em_corr),
+    pH_em_corr_miss_values = all_complete(pH_em_corr),
+
+    # Rules for O2_mmHg
+    O2_mmHg_numeric = is.numeric(O2_mmHg),
+    O2_mmHg_corr_miss_values = all_complete(O2_mmHg),
+
+    # Rules for pH
+    pH_numeric = is.numeric(pH),
+    pH_miss_values = all_complete(pH),
+
+    # Rules for pH_em_corr_corr
+    pH_em_corr_corr_numeric = is.numeric(pH_em_corr_corr),
+    pH_em_corr_corr_miss_values = all_complete(pH_em_corr_corr),
+
+    # # Rules for O2_em_corr_bkg
+    O2_em_corr_bkg_numeric = is.numeric(O2_em_corr_bkg),
+    O2_em_corr_bkg_miss_values = all_complete(O2_em_corr_bkg),
+
+    # Rules for pH_em_corr_bkg
+    pH_em_corr_bkg_numeric = is.numeric(pH_em_corr_bkg),
+    pH_em_corr_bkg_miss_values = all_complete(pH_em_corr_bkg),
+
+    # Rules for O2_mmHg_bkg
+    O2_mmHg_bkg_numeric = is.numeric(O2_mmHg_bkg),
+    O2_mmHg_bkg_miss_values = all_complete(O2_mmHg_bkg),
+
+    # Rules for pH_bkgd
+    pH_bkg_numeric = is.numeric(pH_bkgd),
+    ph_bkg_miss_values = all_complete(pH_bkgd),
+
+    # Rules for pH_em_corr_corr_bkg
+    pH_em_corr_corr_bkg_numeric = is.numeric(pH_em_corr_corr_bkg),
+    pH_em_corr_corr_bkg_miss_values = all_complete(pH_em_corr_corr_bkg)
+  )
+
+# Rules for prepossessed seahorse data, "assay_info" tibble.
+xf_plate_pr_assay_info_rules <- validator(
+  # Rules for F0
+  F0_double = is.double(F0),
+  F0_miss_values = all_complete(F0),
+  F0_min_1 = F0 >= 1,
+
+  # Rules for V_C
+  V_C_double = is.double(V_C),
+  VC_miss_values = all_complete(V_C),
+
+  # Rules for O2_targetEmission
+  O2_targetEmission_double = is.double(O2_targetEmission),
+  O2_targetEmission_miss_values =  all_complete(O2_targetEmission),
+
+  # Rules for Tau_AC
+  Tau_AUC_double = is.double(Tau_AC),
+  Tau_AC_miss_values = all_complete(Tau_AC),
+
+  # Rules for Tau_W
+  Tau_W_double = is.double(Tau_W),
+  Tau_W_miss_values = all_complete(Tau_W),
+
+  # Rules for Tau_C
+  Tau_C_double = is.double(Tau_C),
+  Tau_C_miss_values = all_complete(Tau_C),
+
+  # Rules for Tau_P
+  Tau_P_double = is.double(Tau_P),
+  Tau_P_miss_values = all_complete(Tau_P),
+
+  # Rules for KSV
+  KSV_double = is.double(KSV),
+  KSV_miss_values = all_complete(KSV),
+
+  # Rules for gain1
+  gain1_double = is.double(gain1),
+  gain_miss_values = all_complete(gain1),
+
+  # Rules for gain2
+  gain_2_double = is.double(gain2),
+  gain_2_miss_values = all_complete(gain2),
+
+  # Rules for pH_0
+  pH_0_double = is.double(pH_0),
+  pH_0_miss_values = all_complete(pH_0),
+
+  # Rules for pH_targetEmission
+  pH_targetEmission_double = is.double(pH_targetEmission),
+  pH_targetEmission_miss_values = all_complete(pH_targetEmission),
+
+  # Rules for plate_id
+  plate_id_character = is.character(plate_id),
+  plate_id_miss_values = all_complete(plate_id),
+
+  # Rules for pH_targetEmission
+  pH_targetEmission_double = is.double(pH_targetEmission),
+  pH_targetEmission_miss_values = all_complete(pH_targetEmission),
+
+  # Rules for O2_0_mmHg
+  O2_0_mmHg_double = is.double(O2_0_mmHg),
+  O2_0_mmHg_miss_values = all_complete(O2_0_mmHg),
+
+  # Rules for O2_0_mM
+  O2_0_mM_double = is.double(O2_0_mM),
+  O2_0_mM_miss_values = all_complete(O2_0_mM)
+)
+
+  # Create a dataframe with rules
+  xf_plate_pr_raw_df_rules <- as.data.frame(xf_plate_pr_raw_rules)
+  xf_plate_pr_assay_info_df_rules <- as.data.frame(xf_plate_pr_assay_info_rules)
+
+  # Get key (rule name) and give a specific id
+  rule_id_xf_plate_pr_raw <- add_rule_id(xf_plate_pr_raw_df_rules)
+  rule_id_xf_plate_pr_assay_info <- add_rule_id(xf_plate_pr_assay_info_df_rules)
+
+  ### Add label and description for well character rule ####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl = "well character",
+                  descr = "Well must be type character",
+                  rule_id_xf_plate_pr_raw$well_character)
+
+  ### Add label and description for well field length rule ####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="well field length",
+                  descr="Well has a field length of 3.",
+                  rule_id_xf_plate_pr_raw$well_field_length)
+
+  ### Add label and description for well mssing values rule ####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="well_missing_values",
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$well_miss_val)
+
+  ### Add label and description for well numeric rule ####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="well number",
+                  descr="Well must contain A-H and must be within range 1-12.",
+                  rule_id_xf_plate_pr_raw$well_number)
+
+#   ###measurement####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="measurement",
+                  descr="Measurment has to be integer.",
+                  rule_id_xf_plate_pr_raw$measurement_integer)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='measurement min 1',
+                  descr="Check if measurement has minimum of 1.",
+                  rule_id_xf_plate_pr_raw$measurement_min_1)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='measurment_missing_values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$measurement_miss_val)
+
+# ###tick####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="tick numeric",
+                  descr="Tick must be numeric.",
+                  rule_id_xf_plate_pr_raw$tick_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="tick_min 0",
+                  descr="Check if tick is minimum 0 (if it's below 0 give an error).",
+                  rule_id_xf_plate_pr_raw$tick_min_0)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="tick missing values",
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$tick_miss_val)
+
+# ###timescale####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="timescale numeric",
+                  descr="Timescale must be numeric.",
+                  rule_id_xf_plate_pr_raw$timescale_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="timescale missing values",
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$timescale_miss_val)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="timescale_min_0",
+                  descr="Check if timescale has minimum of 1.",
+                  rule_id_xf_plate_pr_raw$timescale_min_0)
+
+# ###minutes####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="minutes numeric",
+                  descr="Minutes must be numeric",
+                  rule_id_xf_plate_pr_raw$minutes_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="minutes missing values",
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$minutes_miss_values)
+
+# ###group####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="group character",
+                  descr="Group must be character",
+                  rule_id_xf_plate_pr_raw$group_character)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="group missing values",
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$group_miss_values)
+
+# ###interval####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="interval numeric",
+                  descr="Interval has to be numeric.",
+                  rule_id_xf_plate_pr_raw$interval_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl="interval missing values",
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$interval_miss_values)
+
+# ###injection####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='injection_character',
+                  descr="Injection has to be character.",
+                  rule_id_xf_plate_pr_raw$injection_character)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='injection missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$injection_miss_values)
+
+# ###O2_em_corr####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='O2_em_corr numeric',
+                  descr="O2_em_corr has to be numeric",
+                  rule_id_xf_plate_pr_raw$O2_em_corr_numeric)
+
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='O2_em_corr miss values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$O2_em_corr_miss_values)
+
+# ###pH_em_corr####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_em_corr numeric',
+                  descr="pH_em_corr has to be numeric",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_em_corr missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_miss_values)
+
+# ###O2_mmHg####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='O2_mmHg numeric',
+                  descr="O2_mmHg has to be numeric",
+                  rule_id_xf_plate_pr_raw$O2_mmHg_numeric)
+
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='O2_mmHg missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$O2_mmHg_corr_miss_values)
+
+# ###pH####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH numeric',
+                  descr="pH has to be numeric",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_numeric)
+
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_miss_values)
+
+# ###pH_em_corr_corr####
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_em_corr_corr numeric',
+                  descr="pH_em_corr_corr has to be numeric",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_corr_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_em_corr_corr missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_corr_miss_values)
+
+# ###O2_em_corr_bkg####
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='O2_em_corr_bkg numeric',
+                  descr="O2_em_corr_bkg has to be numeric",
+                  rule_id_xf_plate_pr_raw$O2_em_corr_bkg_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='O2_em_corr_bkg missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$O2_em_corr_bkg_miss_values)
+
+# ###pH_em_corr_bkg####
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_em_corr_bkg numeric',
+                  descr="pH_em_corr_bkg has to be numeric",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_bkg_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_em_corr_bkg missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_bkg_miss_values)
+
+# ###O2_mmHg_bkg####
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='O2_mmHg_bkg numeric',
+                  descr="O2_mmHg_bkg has to be numeric",
+                  rule_id_xf_plate_pr_raw$O2_mmHg_bkg_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='O2_mmHg_bkg missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$O2_mmHg_bkg_miss_values)
+
+# ###pH_bkgd####
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_bkgd numeric',
+                  descr="pH_bkgd has to be numeric",
+                  rule_id_xf_plate_pr_raw$pH_bkg_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_bkgd missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$ph_bkg_miss_values)
+
+
+# ###pH_em_corr_corr_bkg####
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_em_corr_corr_bkg numeric',
+                  descr="pH_em_corr_corr_bk has to be numeric",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_corr_bkg_numeric)
+
+rules_description(xf_plate_pr_raw_rules,
+                  lbl='pH_em_corr_corr_bkg missing values',
+                  descr="The functions all_complete() test for missing values or combinations thereof in records.",
+                  rule_id_xf_plate_pr_raw$pH_em_corr_corr_bkg_miss_values)
+
+#' Validate preprocessed data.
+#'
+#' @param xf_plate_pr A preprocessed "Raw" data list (tibble).
+#' @description Check the preprocessed "Raw" data list against validation rules.
+#' @return TRUE when all validation steps pass. FALSE when there is a failure.
+#'
+#' @examples validate_xf_plate_pr(xf_plate_pr)
+
+validate_xf_plate_pr <- function(xf_plate_pr){
+  tryCatch({
+      if (exists("xf_plate_pr") && is.data.frame(get("xf_plate_pr")) == TRUE) {
+
+        source(rstudioapi::getSourceEditorContext()$path)
+
+        # get name for yaml file and pdf validation file.
+        val_name_list <- get_val_name(xf_plate_pr)
+        val_yaml_xf_pr_raw <- val_name_list[1]
+        val_yaml_xf_pr_assay_info <- val_name_list[2]
+
+        # export rules to yaml file.
+        export_yaml(xf_plate_pr_raw_rules, file=here::here("assertions", glue::glue("{val_yaml_xf_pr_raw}.yaml")))
+        export_yaml(xf_plate_pr_assay_info_rules ,file=here::here("assertions", glue::glue("{val_yaml_xf_pr_assay_info}.yaml")))
+
+        # validate/assert a given dataset.
+        raw_validation_output <- validate_yaml_rules(here::here("assertions", glue::glue("{val_yaml_xf_pr_raw}.yaml")),
+                                                     xf_plate_pr$raw_data[[1]])
+        assay_info_validation_output <- validate_yaml_rules(here::here("assertions", glue::glue("{val_yaml_xf_pr_assay_info}.yaml")),
+                                                     xf_plate_pr$assay_info[[1]])
+
+        # check if plate_id is right format
+        plate_id_rules <- validator(grepl("^V[0-9]{10}V$", plate_id))
+        confront(xf_plate_pr, plate_id_rules)
+
+        # summarize validation output.
+        validation_summary_raw <- summarise_out(raw_validation_output)
+        validation_summary_assay_info <- summarise_out(assay_info_validation_output)
+
+        # plot validation output
+        plot_validation_output(raw_validation_output,
+                               here::here("assertions",
+                                          glue::glue("{val_yaml_xf_pr_raw}.pdf")))
+        plot_validation_output(assay_info_validation_output,
+                               here::here("assertions",
+                                          glue::glue("{val_yaml_xf_pr_assay_info}.pdf")))
+
+
+        # collect all information and put in list.
+        validate_xf_plate_pr_list = list()
+        validate_xf_plate_pr_list[[ "assay_info_pr_val" ]] <- list(meta(xf_plate_pr_assay_info_rules),
+                                                                   summary(xf_plate_pr_assay_info_rules))
+
+        validate_xf_plate_pr_list[[ "raw_pr_val" ]] <- list(meta(xf_plate_pr_raw_rules),
+                                                            summary(xf_plate_pr_raw_rules))
+
+
+        validation_summary_list <- list(validation_summary_raw, validation_summary_assay_info)
+
+
+        # check if no validation failures occur.
+        val_true_false <- c()
+        for(summary in validation_summary_list){
+          check_all_true <- check_all_true(summary)
+          check_all_true <- all(check_all_true)
+          val_true_false <- append(val_true_false, check_all_true)
+        }
+
+        # can be true or false, depending on validation failures.
+        validation_pass <- as.logical(all(val_true_false))
+
+        # If no validation failures occur, return true, else return false.
+        return(validation_pass)
+      }
+    },
+
+    warning = function(war) {
+      cat("WARNING :", conditionMessage(war), "\n")
+      logger::log_warn(conditionMessage(war), "\n")
+    },
+    error = function(err) {
+      cat("ERROR :", conditionMessage(err), "\n")
+      logger::log_error(conditionMessage(err), "\n")
+      logger::log_info(glue::glue("Quiting analysis with sheet: {filepath_seahorse}"))
+      break
+    }
+
+)
+}
+
+
+check_all_true <- function(validate_summary){
+  by(validate_summary[4], seq_len(nrow(validate_summary[4])),
+     function(row){
+       if(row[1] == 0){return(TRUE)}
+       else if(row[1] == 1){
+         return(FALSE)
+       }
+     }
+  )
+}
+
+
+
