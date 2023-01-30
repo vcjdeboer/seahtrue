3e72856e20914178ac25a5462312ed6b905f2908
Gerwin Smits <smitsg@ziggo.nl>
2023-01-30 10:24:40 +0000

    feat: Add function for background qc scores.

--- R/process_background_wells.R
+++ R/process_background_wells.R
@@ -6,6 +6,7 @@
 #' @keywords internal
 #' @return AUC_bkgd background parameters for area under the curve (in list)
 get_BKGD_auc <- function(total_df, var, targetEMS) {
+
   background_drift <- function(x, y) {
     y <- y / min(y) - 1
     auc_func <- bayestestR::area_under_curve(x,
@@ -15,6 +16,7 @@ get_BKGD_auc <- function(total_df, var, targetEMS) {
     auc_func <- auc_func / max(x)
     return(auc_func)
   }
+
   background_drift2 <- function(x,
                                 y,
                                 baseline) {
@@ -74,4 +76,45 @@ get_BKGD_auc <- function(total_df, var, targetEMS) {
     }
   }
   return(AUC_bkgd)
+}
+
+get_bkgd_qc_scores <- function(my_plate_df, qc_well_ref, qc_plate_ref, score_cutoff) {
+
+  #calculate auc, auc2 and dev_from_target for Group Background
+  bkgd_qc_target <- get_BKGD_auc(my_plate_df,
+                                 var = "O2_em_corr",
+                                 targetEMS = 12500)
+
+  #calculate stat summaries for NEW target plate - plate
+  qc_plate_target <- bkgd_qc_target %>%
+    summarize(Maximum = max(dev_fromTarget),
+              Minimum = min(dev_fromTarget),
+              'Range(F-L)' = Maximum-Minimum)
+
+  #calculate stat summaries for NEW target plate - well
+  qc_well_target <- bkgd_qc_target %>%
+    dplyr::group_by(well) %>%
+    dplyr::summarize(Maximum = max(dev_fromTarget),
+              Minimum = min(dev_fromTarget),
+              First = first(dev_fromTarget),
+              Last = last(dev_fromTarget),
+              'Range(M-M)' = Maximum-Minimum,
+              'Range(F-L)' = Last-First)
+
+  #calculate scores for a plate
+  well_scores_target <- get_well_scores(qc_well_target, qc_well_ref) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(well,total_score) #minimum score = 6 -> max = 18
+
+  #calculate scores for wells
+  plate_scores_target <- get_plate_scores(qc_plate_target, qc_plate_ref) %>%
+    dplyr::select(total_score) #minimum score = 3 -> max = 9
+
+  #generate output list
+  total_score_output_list <- list(ref_bkgd_qc = "qc_well_ref_PBMC", #this should be the name of the ref set
+                                  well_scores = well_scores_target,
+                                  plate_scores = plate_scores_target)
+
+  return(total_score_output_list)
+
 }
