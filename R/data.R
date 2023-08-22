055cb69b2b7a71e34c057c7cf4d0bf1ced66a12a
Smits <gerwin.smits@wur.nl>
2023-08-22 14:03:32 +0200

    refactor: Update data.R

--- R/data.R
+++ R/data.R
@@ -8,13 +8,13 @@
 #'
 #' @format A tibble with 1 row and 7 columns:
 #'   \describe{
-#'     \item{plate_id}          {Seahorse Plate ID}                    {character}
-#'     \item{filepath_seahorse} {Path to the Seahorse Wave excel file} {character}
-#'     \item{date}              {Date of the Seahorse run}             {character}
-#'     \item{assay_info}        {Assay information (as a list)}        {tibble: 1 × 25}
-#'     \item{injection_info}    {Injection information (as a list)}    {tibble: 12 × 3}
-#'     \item{raw_data}          {Preprocessed raw data (as a list)}    {tibble: 663,552 × 21}
-#'     \item{rate_data}         {Preprocessed rate data (as a list)}   {tibble: 1,152 × 10}
+#'     \item{plate_id}          {Seahorse Plate ID}
+#'     \item{filepath_seahorse} {Path to the Seahorse Wave excel file}
+#'     \item{date}              {Date of the Seahorse run}
+#'     \item{assay_info}        {Assay information (as a list)}
+#'     \item{injection_info}    {Injection information (as a list)}
+#'     \item{raw_data}          {Preprocessed raw data (as a list)}
+#'     \item{rate_data}         {Preprocessed rate data (as a list)}
 #'   }
 #
 #' @usage run_seahtrue(filepath_seahorse)
@@ -25,39 +25,7 @@
 #' seahtrue_output_donor_A <- seahtrue::run_seahtrue(system.file("extdata", "20191219 SciRep PBMCs donor A.xlsx", package = "seahtrue"))
 #' seahtrue_output_donor_B <- seahtrue::run_seahtrue(system.file("extdata", "20200110 SciRep PBMCs donor B.xlsx", package = "seahtrue"))
 #' seahtrue_output_donor_C <- seahtrue::run_seahtrue(system.file("extdata", "20200110 SciRep PBMCs donor C.xlsx", package = "seahtrue"))
-#'
-#' \dontrun{
-#' save(seahtrue_output_donor_A, file="seahtrue_output_donor_A.rda", compress='xz')
-#' save(seahtrue_output_donor_B, file="seahtrue_output_donor_B.rda", compress='xz')
-#' save(seahtrue_output_donor_C, file="seahtrue_output_donor_C.rda", compress='xz')
-#'
-#' Load the example dataset
-#'
-#' load(here::here("data", "seahtrue_output_donor_A.rda"))
-#' load(here::here("data", "seahtrue_output_donor_A.rda"))
-#' load(here::here("data", "seahtrue_output_donor_A.rda"))
-#'
-#' Display the dataset
-#' print(seahtrue_output_donor_A)
-#' print(seahtrue_output_donor_B)
-#' print(seahtrue_output_donor_C)
-#' }
-
-
-#' Seahtrue Output Dataset for Donor B
-#'
-#' This dataset contains the output of the Seahtrue analysis for donor B.
-#'
-#' @name seahtrue_output_donor_B
-#' @title Seahtrue output for donor B.
-#' @description The dataset represents the preprocessed Seahorse information for donor B.
-#' ...
-#'
-#' @format A tibble with appropriate columns and descriptions.
-#'
-#' @usage data(seahtrue_output_donor_B)
-"seahtrue_output_donor_B"
-
+#' 
 #' Seahtrue Output Dataset for Donor A
 #'
 #' This dataset contains the output of the Seahtrue analysis for donor A.
@@ -70,8 +38,20 @@
 #' @format A tibble with appropriate columns and descriptions.
 #'
 #' @usage data(seahtrue_output_donor_A)
-"seahtrue_output_donor_A"
-
+#' 
+#' Seahtrue Output Dataset for Donor B
+#'
+#' This dataset contains the output of the Seahtrue analysis for donor B.
+#'
+#' @name seahtrue_output_donor_B
+#' @title Seahtrue output for donor B.
+#' @description The dataset represents the preprocessed Seahorse information for donor B.
+#' ...
+#'
+#' @format A tibble with appropriate columns and descriptions.
+#'
+#' @usage data(seahtrue_output_donor_B)
+#' 
 #' Seahtrue Output Dataset for Donor C
 #'
 #' This dataset contains the output of the Seahtrue analysis for donor C.
@@ -84,4 +64,3 @@
 #' @format A tibble with appropriate columns and descriptions.
 #'
 #' @usage data(seahtrue_output_donor_C)
-"seahtrue_output_donor_C"
