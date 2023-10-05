b09ed7c61f12d7627b60d0f906729ea4966dbc5a
Smits <gerwin.smits@wur.nl>
2023-10-05 11:58:41 +0200

    refactor: Refator datasets documentation.

--- R/data.R
+++ R/data.R
@@ -1,74 +1,56 @@
-#' Example Dataset from run_seahtrue Function
+#' Dataset donor A
 #'
-#' This dataset is an example output of the run_seahtrue function.
+#' A dataset containing output for donor A. This data contains the preprocess 
+#' output that was generated during the run of our Seahtrue pipeline. This data was 
+#' created with generated with input file '20191219 SciRep PBMCs donor A.xlsx', 
+#' located at the inst/extdata directory.
 #'
-#' @name run_seahtrue_example
-#' @title Example of running the run_seahtrue function.
-#' @description A final output datset for seahtrue created with run_seahtrue().
+#' @format A data frame with 1 rows and 7 variables.
+#' \describe{
+#'   \item{plate_id}{Barcode from the 'Assay Configuration' sheet from the Seahorse Excel file.}
+#'   \item{filepath_seahorse}{Path to Seahorse Excel file.}
+#'   \item{date}{Date derived from 'Assay Configuration' sheet of the Seahorse Excel file.}
+#'   \item{assay_info}{Date derived from 'Assay Configuration' sheet of the Seahorse Excel file.}
+#'   \item{injection_info}{Dataframe with information from the 'Assay Configuration' sheet from the Seahorse Excel file.}
+#'   \item{raw_data}{Preprocessed raw dataframe from Seahorse 'Raw' sheet.}
+#'   \item{rate_data}{Preprocessed rate data from 'Rate' sheet.}
+#' }
+"seahtrue_output_donor_A"
+
+#' Dataset donor B
 #'
-#' @format A tibble with 1 row and 7 columns:
-#'   \describe{
-#'     \item{plate_id}          {Seahorse Plate ID}
-#'     \item{filepath_seahorse} {Path to the Seahorse Wave excel file}
-#'     \item{date}              {Date of the Seahorse run}
-#'     \item{assay_info}        {Assay information (as a list)}
-#'     \item{injection_info}    {Injection information (as a list)}
-#'     \item{raw_data}          {Preprocessed raw data (as a list)}
-#'     \item{rate_data}         {Preprocessed rate data (as a list)}
-#'   }
-#
-#' @usage run_seahtrue(filepath_seahorse)
-#' @param dataset A data frame containing Seahorse data.
-#' @return A data frame with preprocessed Seahorse information.
+#' A dataset containing output for donor A. This data contains the preprocess 
+#' output that was generated during the run of our Seahtrue pipeline. This data was 
+#' created with generated with input file '20200110 SciRep PBMCs donor B.xlsx', 
+#' located at the inst/extdata directory.
 #'
-#' @examples
-#' seahtrue_output_donor_A <- seahtrue::run_seahtrue(system.file("extdata", "20191219 SciRep PBMCs donor A.xlsx", package = "seahtrue"))
-#' seahtrue_output_donor_B <- seahtrue::run_seahtrue(system.file("extdata", "20200110 SciRep PBMCs donor B.xlsx", package = "seahtrue"))
-#' seahtrue_output_donor_C <- seahtrue::run_seahtrue(system.file("extdata", "20200110 SciRep PBMCs donor C.xlsx", package = "seahtrue"))
-#' 
-#' Seahtrue Output Dataset for Donor A
+#' @format A data frame with 1 rows and 7 variables:
+#' \describe{
+#'   \item{plate_id}{Barcode from the 'Assay Configuration' sheet from the Seahorse Excel file.}
+#'   \item{filepath_seahorse}{Path to Seahorse Excel file.}
+#'   \item{date}{Date derived from 'Assay Configuration' sheet of the Seahorse Excel file.}
+#'   \item{assay_info}{Date derived from 'Assay Configuration' sheet of the Seahorse Excel file.}
+#'   \item{injection_info}{Dataframe with information from the 'Assay Configuration' sheet from the Seahorse Excel file.}
+#'   \item{raw_data}{Preprocessed raw dataframe from Seahorse 'Raw' sheet.}
+#'   \item{rate_data}{Preprocessed rate data from 'Rate' sheet.}
+#' }
+"seahtrue_output_donor_B"
+
+#' Dataset donor C
 #'
-#' This dataset contains the output of the Seahtrue analysis for donor A.
+#' A dataset containing output for donor A. This data contains the preprocess 
+#' output that was generated during the run of our Seahtrue pipeline. This data was 
+#' created with generated with input file '20200110 SciRep PBMCs donor C.xlsx', 
+#' located at the inst/extdata directory.
 #'
-#' @name seahtrue_output_donor_A
-#' @title Seahtrue output for donor A.
-#' @description The dataset represents the preprocessed Seahorse information for donor A.
-#' ...
-#'
-#' @format A tibble with appropriate columns and descriptions.
-#'
-#' @usage data(seahtrue_output_donor_A)
-#' 
-#' Seahtrue Output Dataset for Donor B
-#'
-#' This dataset contains the output of the Seahtrue analysis for donor B.
-#'
-#' @name seahtrue_output_donor_B
-#' @title Seahtrue output for donor B.
-#' @description The dataset represents the preprocessed Seahorse information for donor B.
-#' ...
-#'
-#' @format A tibble with appropriate columns and descriptions.
-#'
-#' @usage data(seahtrue_output_donor_B)
-#' 
-#' Seahtrue Output Dataset for Donor C
-#'
-#' This dataset contains the output of the Seahtrue analysis for donor C.
-#'
-#' @name seahtrue_output_donor_C
-#' @title Seahtrue output for donor C.
-#' @description The dataset represents the preprocessed Seahorse information for donor C.
-#' ...
-#'
-#' @format A tibble with appropriate columns and descriptions.
-#'
-#' @usage data(seahtrue_output_donor_C)
-#' 
-#' 
-#' @name create_yaml_location
-#' @title Make yaml location available
-#' @description Make YAML location available for for preprocessing validation.
-#' 
-#' @format YAML file
-#' create_raw_yaml(xf_plate_pr_raw_rules, system.file("R", "assertions", "rules_assay_info.yaml", package = "seahtrue"))
+#' @format A data frame with 1 rows and 7 variables:
+#' \describe{
+#'   \item{plate_id}{Barcode from the 'Assay Configuration' sheet from the Seahorse Excel file.}
+#'   \item{filepath_seahorse}{Path to Seahorse Excel file.}
+#'   \item{date}{Date derived from 'Assay Configuration' sheet of the Seahorse Excel file.}
+#'   \item{assay_info}{Date derived from 'Assay Configuration' sheet of the Seahorse Excel file.}
+#'   \item{injection_info}{Dataframe with information from the 'Assay Configuration' sheet from the Seahorse Excel file.}
+#'   \item{raw_data}{Preprocessed raw dataframe from Seahorse 'Raw' sheet.}
+#'   \item{rate_data}{Preprocessed rate data from 'Rate' sheet.}
+#' }
+"seahtrue_output_donor_C"
