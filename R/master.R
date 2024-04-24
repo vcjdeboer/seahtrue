b0ad7551dc5b0d6b36b4ab07ded8caa112a9444f
Vincent de Boer <vcjdeboer@gmail.com>
2024-04-24 08:41:04 +0200

    style: line length to 80 characters

--- R/master.R
+++ R/master.R
@@ -16,66 +16,75 @@
 #'  * raw_data = Preprocessed raw dataframe from 'Raw' sheet \cr
 #'  * rate_data = Preprocessed rate data from 'Rate' sheet \cr
 #'  # validation_output = Output of the data checks. including rules
-#'  
+#'
 #' @export
-#' @import logger rlang cli glue readxl 
+#' @import logger rlang cli glue readxl knitr sessioninfo
 #' @family {read functions}
 #' @examples
 #' revive_xfplate(
-#'   system.file("extdata", 
-#'   "20191219_SciRep_PBMCs_donor_A.xlsx", 
-#'   package = "seahtrue"))
+#'     system.file("extdata",
+#'         "20191219_SciRep_PBMCs_donor_A.xlsx",
+#'         package = "seahtrue"
+#'     )
+#' )
 #'
 revive_xfplate <- function(filepath_seahorse) {
+    logger::log_info("Start reviving")
 
-  logger::log_info("Start reviving")
-  
-  # Check if argument is present
-  rlang::check_required(filepath_seahorse)
+    # Check if argument is present
+    rlang::check_required(filepath_seahorse)
 
-  # Check if file exists.
-  if (!file.exists(filepath_seahorse)) {
-    cli::cli_abort(
-      glue::glue("The following file 
-                  does not exist: {basename(filepath_seahorse)}"),
-      wrap = TRUE)}
-  
-  # Check if required sheets are available in input file
-  sheets_required <- c("Assay Configuration",
-                       "Rate",
-                       "Raw",
-                       "Calibration",
-                       "Operation Log")
-  my_excel_sheets <- 
-    readxl::excel_sheets(filepath_seahorse) 
-  
-  my_missing_sheets <- missing_strings(my_excel_sheets, 
-                                       sheets_required)
-  
-  if (!is.null(my_missing_sheets)){
-    if (length(my_missing_sheets)>1){
-    cli::cli_abort(
-      glue::glue("The following sheets 
-                  do not exist: {my_missing_sheets}"),
-      wrap = TRUE)
-      } else {
+    # Check if file exists.
+    if (!file.exists(filepath_seahorse)) {
         cli::cli_abort(
-          glue::glue("The following sheet 
-                      does not exist: {my_missing_sheets}"),
-          wrap = TRUE)
+            glue::glue("The following file 
+                  does not exist: {basename(filepath_seahorse)}"),
+            wrap = TRUE
+        )
     }
-  }
-  
-  # run the read/preprocess/validate function
-  preprocessed_xf_plate <- filepath_seahorse %>%
-    read_xfplate() %>%
-    preprocess_xfplate() %>% 
-    validate_preprocessed()
-  
-  logger::log_info("Finished reviving")
-  
-  
-  return(preprocessed_xf_plate)
+
+    # Check if required sheets are available in input file
+    sheets_required <- c(
+        "Assay Configuration",
+        "Rate",
+        "Raw",
+        "Calibration",
+        "Operation Log"
+    )
+    my_excel_sheets <-
+        readxl::excel_sheets(filepath_seahorse)
+
+    my_missing_sheets <- missing_strings(
+        my_excel_sheets,
+        sheets_required
+    )
+
+    if (!is.null(my_missing_sheets)) {
+        if (length(my_missing_sheets) > 1) {
+            cli::cli_abort(
+                glue::glue("The following sheets 
+                  do not exist: {my_missing_sheets}"),
+                wrap = TRUE
+            )
+        } else {
+            cli::cli_abort(
+                glue::glue("The following sheet 
+                      does not exist: {my_missing_sheets}"),
+                wrap = TRUE
+            )
+        }
+    }
+
+    # run the read/preprocess/validate function
+    preprocessed_xf_plate <- filepath_seahorse %>%
+        read_xfplate() %>%
+        preprocess_xfplate() %>%
+        validate_preprocessed()
+
+    logger::log_info("Finished reviving")
+
+
+    return(preprocessed_xf_plate)
 }
 
 #' Glueing mulltiple plates from a folder
@@ -83,7 +92,7 @@ revive_xfplate <- function(filepath_seahorse) {
 #' @description
 #' This function takes a folder path and on the available .xlsx files
 #' the revive_xfplate() function is run and output in one nested tibble.
-#' 
+#'
 #' @param folderpath_seahorse the path to a folder where the .xlsx files
 #' are located or a vector of strings pointing to the path of each individual file
 #' @param arg_is_folder either TRUE or FALSE. When the input is a vector of path strings
@@ -92,57 +101,60 @@ revive_xfplate <- function(filepath_seahorse) {
 #' @family {read functions}
 #' @export
 #' @examples
-#' c(system.file("extdata",
-#'   "20191219_SciRep_PBMCs_donor_A.xlsx", 
-#'   package = "seahtrue"),
-#'   system.file("extdata",
-#'   "20200110_SciRep_PBMCs_donor_B.xlsx", 
-#'   package = "seahtrue"),
-#'   system.file("extdata",
-#'   "20200110_SciRep_PBMCs_donor_C.xlsx", 
-#'   package = "seahtrue")) |> 
-#'   glue_xfplates(arg_is_folder = FALSE)
-glue_xfplates <- function(folderpath_seahorse, arg_is_folder){
-  
-  logger::log_info("Start glueing")
-  rlang::check_required(folderpath_seahorse)
-  
-  if(arg_is_folder){
-    if (!dir.exists(folderpath_seahorse)) {
-      cli::cli_abort(
-        glue::glue("The following folder 
+#' c(
+#'     system.file("extdata",
+#'         "20191219_SciRep_PBMCs_donor_A.xlsx",
+#'         package = "seahtrue"
+#'     ),
+#'     system.file("extdata",
+#'         "20200110_SciRep_PBMCs_donor_B.xlsx",
+#'         package = "seahtrue"
+#'     ),
+#'     system.file("extdata",
+#'         "20200110_SciRep_PBMCs_donor_C.xlsx",
+#'         package = "seahtrue"
+#'     )
+#' ) |>
+#'     glue_xfplates(arg_is_folder = FALSE)
+glue_xfplates <- function(folderpath_seahorse, arg_is_folder) {
+    logger::log_info("Start glueing")
+    rlang::check_required(folderpath_seahorse)
+
+    if (arg_is_folder) {
+        if (!dir.exists(folderpath_seahorse)) {
+            cli::cli_abort(
+                glue::glue("The following folder 
                     does not exist: 
                    {basename(folderpath_seahorse)}"),
-        wrap = TRUE)}
-    
-    file_list <- list.files(folderpath_seahorse,
-                            pattern='*.xlsx', 
-                            full.names = TRUE) %>% 
-            stringr::str_subset(., 
-                          fixed('~$'), negate = TRUE)
-  } else {
-    file_list <- folderpath_seahorse
-  }
-  
-  if(is.null(file_list)){
-    cli::cli_abort(
-      glue::glue("There are zero .xlsx files in 
+                wrap = TRUE
+            )
+        }
+
+        file_list <- list.files(folderpath_seahorse,
+            pattern = "*.xlsx",
+            full.names = TRUE
+        ) %>%
+            stringr::str_subset(.,
+                fixed("~$"),
+                negate = TRUE
+            )
+    } else {
+        file_list <- folderpath_seahorse
+    }
+
+    if (is.null(file_list)) {
+        cli::cli_abort(
+            glue::glue("There are zero .xlsx files in 
                  {(folderpath_seahorse)}"),
-      wrap = TRUE)
-  }
-  
-  df <- file_list %>% 
-    purrr::map(~ .x %>% revive_xfplate()) %>% 
-    purrr::list_rbind()
-  
-  logger::log_info("Finished glueing")
-  
-  return(df)
-  
+            wrap = TRUE
+        )
+    }
+
+    df <- file_list %>%
+        purrr::map(~ .x %>% revive_xfplate()) %>%
+        purrr::list_rbind()
+
+    logger::log_info("Finished glueing")
+
+    return(df)
 }
-
-
-
-
-
-
--- R/preprocess_xfplate.R
+++ R/preprocess_xfplate.R
@@ -2,12 +2,14 @@
 #' Preprocessing the seahorse plate data that was read in read_xfplate.r
 #'
 #' @description
-#' This function performs the preprocessing of the xf_raw and the xf_rate tibbles
+#' This function performs the preprocessing of the xf_raw and the xf_rate 
+#' tibbles
 #' and generates the final output nested tibble
 #'
 #' @param xf list with all necessary seahorse data.
 #'
-#' @return xf_plate_pr is returned this is a nested tibble with the following 7 columns:
+#' @return xf_plate_pr is returned this is a nested tibble with the following 
+#' 7 columns:
 #'  * plate_id = plate_id (from assay configuration sheet)
 #'  * filepath_seahorse = the original input file and its path on local drive
 #'  * date = date that seahorse was run (from assay configuration sheet)
@@ -22,49 +24,56 @@
 #'
 #' @examples
 #' preprocess_xfplate(xf_donor_A)
+preprocess_xfplate <- function(xf) {
+    xf_raw_pr <- preprocess_xf_raw(
+        xf$raw,
+        xf$pHcal,
+        xf$inj,
+        xf$assayinfo,
+        xf$buffer,
+        xf$norm,
+        xf$flagged
+    )
 
-preprocess_xfplate <- function(xf){
-  
-  xf_raw_pr <- preprocess_xf_raw(xf$raw,
-                                 xf$pHcal,
-                                 xf$inj,
-                                 xf$assayinfo,
-                                 xf$buffer,
-                                 xf$norm,
-                                 xf$flagged)
+    xf_rate_pr <- preprocess_xf_rate(
+        xf$rate,
+        xf$norm,
+        xf$inj,
+        xf$flagged
+    )
 
-  xf_rate_pr <- preprocess_xf_rate(xf$rate,
-                                   xf$norm,
-                                   xf$inj,
-                                   xf$flagged)
+    xf_plate_pr <- xf_raw_pr %>%
+        tidyr::nest(.by = plate_id) %>%
+        dplyr::mutate(
+            filepath_seahorse = list(tibble::tibble(
+                directory_path = dirname(xf$filepath_seahorse),
+                base_name = basename(xf$filepath_seahorse),
+                full_path = xf$filepath_seahorse
+            )),
+            date_run = xf$assayinfo$date_run,
+            date_processed = Sys.time(),
+            assay_info = list(tibble::tibble(xf$assayinfo)),
+            rate_data = list(tibble::tibble(xf_rate_pr)),
+            injection_info = list(tibble::tibble(xf$inj))
+        ) %>%
+        dplyr::select(plate_id, filepath_seahorse,
+            date_run, date_processed, assay_info,
+            injection_info,
+            raw_data = data, rate_data
+        )
 
-  xf_plate_pr <- xf_raw_pr %>%
-    tidyr::nest(.by = plate_id) %>%
-    dplyr::mutate(
-      filepath_seahorse = list(tibble::tibble(
-      directory_path = dirname(xf$filepath_seahorse),
-      base_name = basename(xf$filepath_seahorse),
-      full_path = xf$filepath_seahorse)),
-      date_run = xf$assayinfo$date_run,
-      date_processed = Sys.time(),
-      assay_info = list(tibble::tibble(xf$assayinfo)),
-      rate_data = list(tibble::tibble(xf_rate_pr)),
-      injection_info = list(tibble::tibble(xf$inj))) %>%
-    dplyr::select(plate_id, filepath_seahorse, 
-                  date_run, date_processed, assay_info, 
-                  injection_info,
-                  raw_data = data, rate_data)
-  
-  cli::cli_alert_info("Finished preprocessing of the input data")
+    cli::cli_alert_info("Finished preprocessing of the input data")
 
-  return(xf_plate_pr)
+    return(xf_plate_pr)
 }
 
 #' Preprocess xf raw
 #'
 #' @description
-#' This function uses seahorse excel data that was read using functions from read_xfplate.
-#' The function will preprocess and merge data into the xf_raw data tibble, to produce
+#' This function uses seahorse excel data that was read using functions 
+#' from read_xfplate.
+#' The function will preprocess and merge data into the xf_raw data tibble, 
+#' to produce
 #' the xf_raw_pr tibble by:
 #'   * changing columns names
 #'   * adding new time columns
@@ -75,20 +84,24 @@ preprocess_xfplate <- function(xf){
 #'   * calculating raw pH emission data
 #'
 #' @param xf_raw List (tibble) that contains 'Raw' Seahorse information.
-#' @param xf_pHcal List (tibble) that contains well and the corresponding pH calibration emission info.
+#' @param xf_pHcal List (tibble) that contains well and the corresponding 
+#' pH calibration emission info.
 #' @param xf_inj A list (tibble) that contains injection information.
 #' @param xf_assayinfo List (tibble) with assay information.
 #' @param xf_buffer List (tibble) that contains well and bufferfactor.
-#' @param xf_norm List consisting [1] well names and the corresponding normalization values and
+#' @param xf_norm List consisting [1] well names and the corresponding 
+#' normalization values and
 #' [2] check if normalization data is available (TRUE/FALSE).
 #' @param xf_flagged Vector that contains wells that were "unselected" (flagged).
 #'
-#' @note The returned preprocessed "Raw" tibble doesn't only contain data of the "Raw" sheet,
+#' @note The returned preprocessed "Raw" tibble doesn't only contain 
+#' data of the "Raw" sheet,
 #' but for example also data from the "Assay Configuration sheet".
 #'
 #' @return A preprocessed "Raw" data list (tibble). With the following columns:
 #'
-#' plate_id, well, measurement, tick, timescale, minutes, group, interval, injection,
+#' plate_id, well, measurement, tick, timescale, minutes, group, interval, 
+#' injection,
 #' O2_em_corr, pH_em_corr, O2_mmHg, pH, pH_em_corr_corr, O2_em_corr_bkg,
 #' pH_em_corr_bkg, O2_mmHg_bkg, pH_bkgd, pH_em_corr_corr_bkg, bufferfactor,
 #' cell_n, flagged_well
@@ -97,8 +110,8 @@ preprocess_xfplate <- function(xf){
 #' @keywords internal
 #'
 #' @examples
-#' preprocess_xf_raw(xf_donor_A$raw, xf_donor_A$pHcal, xf_donor_A$inj, xf_donor_A$assayinfo, xf_donor_A$buffer, xf_donor_A$norm, xf_donor_A$flagged)
-
+#' preprocess_xf_raw(xf_donor_A$raw, xf_donor_A$pHcal, xf_donor_A$inj, 
+#' xf_donor_A$assayinfo, xf_donor_A$buffer, xf_donor_A$norm, xf_donor_A$flagged)
 preprocess_xf_raw <- function(xf_raw,
                               xf_pHcal,
                               xf_inj,
@@ -106,73 +119,75 @@ preprocess_xf_raw <- function(xf_raw,
                               xf_buffer,
                               xf_norm,
                               xf_flagged) {
-  
-      
+    # convert time column
+    xf_raw_pr <- convert_timestamp(xf_raw)
 
-  # convert time column
-  xf_raw_pr <- convert_timestamp(xf_raw)
-  
-  #add flag well columnn
-  xf_flagged <- xf_flagged %>%  pull(well)
-  xf_raw_pr$flagged_well <- FALSE
-  xf_raw_pr$flagged_well[xf_raw_pr$well %in% xf_flagged] <- TRUE
+    # add flag well columnn
+    xf_flagged <- xf_flagged %>% pull(well)
+    xf_raw_pr$flagged_well <- FALSE
+    xf_raw_pr$flagged_well[xf_raw_pr$well %in% xf_flagged] <- TRUE
 
-  # correct pH_em_corr
-  xf_raw_pr$pH_em_corr_corr <- 
-    correct_pH_em_corr(xf_raw_pr$pH_em_corr,
-                      xf_pHcal$pH_cal_em,
-                      xf_assayinfo$pH_targetEmission[1])
-
-  
-  # calculate backgrounds and join
-  background <- calc_background(xf_raw_pr)
-  xf_raw_pr <- xf_raw_pr %>%
-    dplyr::left_join(background, by = c("tick"))
-
-  # add injection info
-  xf_raw_pr <- xf_raw_pr %>% 
-    dplyr::left_join(xf_inj, by = "measurement")
-
-  #add plate_id to df
-  xf_raw_pr$plate_id <- xf_assayinfo$plate_id
-
-  #add norm_info
-  xf_raw_pr <- xf_raw_pr %>% 
-    dplyr::left_join(xf_norm, by = c("well"))
-
-  #add bufferfactor
-  xf_raw_pr <- xf_raw_pr %>% 
-    dplyr::left_join(xf_buffer, by = c("well"))
+    # correct pH_em_corr
+    xf_raw_pr$pH_em_corr_corr <-
+        correct_pH_em_corr(
+            xf_raw_pr$pH_em_corr,
+            xf_pHcal$pH_cal_em,
+            xf_assayinfo$pH_targetEmission[1]
+        )
 
 
+    # calculate backgrounds and join
+    background <- calc_background(xf_raw_pr)
+    xf_raw_pr <- xf_raw_pr %>%
+        dplyr::left_join(background, by = c("tick"))
 
-  # select columns that are needed
-  xf_raw_pr <- xf_raw_pr %>% 
-    dplyr::select(
-      plate_id, well, measurement, tick, 
-      timescale, minutes, group, interval, injection,
-      O2_em_corr, pH_em_corr, O2_mmHg, pH, pH_em_corr_corr, 
-      O2_em_corr_bkg, pH_em_corr_bkg, O2_mmHg_bkg, pH_bkgd,
-      pH_em_corr_corr_bkg, bufferfactor, cell_n, flagged_well
-  )
+    # add injection info
+    xf_raw_pr <- xf_raw_pr %>%
+        dplyr::left_join(xf_inj, by = "measurement")
 
-  return(xf_raw_pr)
-  
-  }
+    # add plate_id to df
+    xf_raw_pr$plate_id <- xf_assayinfo$plate_id
+
+    # add norm_info
+    xf_raw_pr <- xf_raw_pr %>%
+        dplyr::left_join(xf_norm, by = c("well"))
+
+    # add bufferfactor
+    xf_raw_pr <- xf_raw_pr %>%
+        dplyr::left_join(xf_buffer, by = c("well"))
+
+
+
+    # select columns that are needed
+    xf_raw_pr <- xf_raw_pr %>%
+        dplyr::select(
+            plate_id, well, measurement, tick,
+            timescale, minutes, group, interval, injection,
+            O2_em_corr, pH_em_corr, O2_mmHg, pH, pH_em_corr_corr,
+            O2_em_corr_bkg, pH_em_corr_bkg, O2_mmHg_bkg, pH_bkgd,
+            pH_em_corr_corr_bkg, bufferfactor, cell_n, flagged_well
+        )
+
+    return(xf_raw_pr)
+}
 
 
 
 #' Preprocess xf_rate
 #'
-#' @description This function edits the xf_rate tibble to create a preprocessed tibble.
+#' @description This function edits the xf_rate tibble to create a preprocessed 
+#' tibble.
 #' The rate df contains OCR and ECAR information from the seahorse excel file.
 #'
-#' @note The final preprocessed tibble contains data from both the seahorse "Rate" sheet and "Assay Configuration" sheet.
+#' @note The final preprocessed tibble contains data from both the seahorse 
+#' "Rate" sheet and "Assay Configuration" sheet.
 #'
 #' @param xf_rate Tibble that contains original rate data tibble
-#' @param xf_norm Tibble consisting of well names and the corresponding normalization values
+#' @param xf_norm Tibble consisting of well names and the corresponding 
+#' normalization values
 #' @param xf_inj Tibble of injection info
-#' @param xf_flagged Tibble that contains wells that were "unselected" (flagged).
+#' @param xf_flagged Tibble that contains wells that were "unselected" 
+#' (flagged).
 #'
 #' @return Preprocessed Rate tibble
 #'
@@ -181,22 +196,21 @@ preprocess_xf_raw <- function(xf_raw,
 #'
 #' @examples
 #' preprocess_xf_rate(xf_donor_A$rate, xf_donor_A$norm, xf_donor_A$flagged)
-
 preprocess_xf_rate <- function(xf_rate,
                                xf_norm,
                                xf_inj,
-                               xf_flagged){
-  #add norm_info to rate data
-  OCR_from_excel <- xf_rate %>% 
-    dplyr::left_join(xf_norm, by = c("well"))
-  
-  OCR_from_excel <- OCR_from_excel %>% 
-    dplyr::left_join(xf_inj, by = c("measurement"))
+                               xf_flagged) {
+    # add norm_info to rate data
+    OCR_from_excel <- xf_rate %>%
+        dplyr::left_join(xf_norm, by = c("well"))
 
-  OCR_from_excel$flagged_well <- FALSE
-  OCR_from_excel$flagged_well[OCR_from_excel$well %in% xf_flagged] <- TRUE
+    OCR_from_excel <- OCR_from_excel %>%
+        dplyr::left_join(xf_inj, by = c("measurement"))
 
-  return(OCR_from_excel)
+    OCR_from_excel$flagged_well <- FALSE
+    OCR_from_excel$flagged_well[OCR_from_excel$well %in% xf_flagged] <- TRUE
+
+    return(OCR_from_excel)
 }
 
 # DEPENDENT FUNCTIONS ---------------------------------------------------------------
@@ -206,7 +220,8 @@ preprocess_xf_rate <- function(xf_rate,
 
 #' Convert timestamp
 #'
-#' @description Convert the the time column in the WAVE input dataframe to a time scale in seconds.
+#' @description Convert the the time column in the WAVE input dataframe 
+#' to a time scale in seconds.
 #'
 #' @note This function is called at the preprocess script.
 #'
@@ -218,19 +233,24 @@ preprocess_xf_rate <- function(xf_rate,
 #' @keywords internal
 #'
 
-convert_timestamp <-  function(xf_raw_pr){
-  
-  xf_raw_pr %>% 
-    mutate(timestamp = as.character(timestamp)) %>% 
-    mutate(times = 
-             stringr::str_split(timestamp, ":")) %>% 
-    mutate(totalMinutes =
-             purrr::map_dbl(times, ~ .x %>% 
-                              as.numeric() %>% 
-                              {first(.)*60 + nth(.,2)+ nth(.,3)/60})) %>%  
-    mutate(minutes = totalMinutes - first(totalMinutes)) %>%
-    mutate(timescale = round(minutes*60)) %>% 
-    select(-times)
+convert_timestamp <- function(xf_raw_pr) {
+    xf_raw_pr %>%
+        mutate(timestamp = as.character(timestamp)) %>%
+        mutate(
+            times =
+                stringr::str_split(timestamp, ":")
+        ) %>%
+        mutate(
+            totalMinutes =
+                purrr::map_dbl(times, ~ .x %>%
+                    as.numeric() %>%
+                    {
+                        first(.) * 60 + nth(., 2) + nth(., 3) / 60
+                    })
+        ) %>%
+        mutate(minutes = totalMinutes - first(totalMinutes)) %>%
+        mutate(timescale = round(minutes * 60)) %>%
+        select(-times)
 }
 
 
@@ -238,22 +258,22 @@ convert_timestamp <-  function(xf_raw_pr){
 ## correct_pH_em_corr() ------------------------------------------------------
 #' Corrected pH emission
 #'
-#' @description Calculate the pH emission corrected for calibration not reaching target emission.
+#' @description Calculate the pH emission corrected for calibration not reaching 
+#' target emission.
 #'
 #' @note This function is called at the preprocess script.
 #'
 #' @param pH_em_corr pH corrected emission derived from the seahorse "Raw" sheet.
 #' @param pH_cal_em pH emission derived from the seahorse "Calibration" sheet.
-#' @param pH_targetEmission pH target emission derived from the seahorse "Calibration" sheet.
+#' @param pH_targetEmission pH target emission derived from the seahorse 
+#' "Calibration" sheet.
 #'
 #' @return a vector with corrected pH_em_corr 'pH_em_corr_corr'
 #' @noRd
 #' @keywords internal
 
-correct_pH_em_corr <- function(pH_em_corr, pH_cal_em, pH_targetEmission){
-
-  correct_pH_em_corr <- (pH_targetEmission / pH_cal_em) * pH_em_corr
-
+correct_pH_em_corr <- function(pH_em_corr, pH_cal_em, pH_targetEmission) {
+    correct_pH_em_corr <- (pH_targetEmission / pH_cal_em) * pH_em_corr
 }
 
 ## calc_background() -------------------------------------------------------
@@ -265,29 +285,31 @@ correct_pH_em_corr <- function(pH_em_corr, pH_cal_em, pH_targetEmission){
 #'
 #' @param xf_raw_pr A list (tibble dataframe) for preprocessing.
 #'
-#' @return A new dataframe  'background' with for each measurement the mean background for:
+#' @return A new dataframe  'background' with for each measurement the mean 
+#' background for:
 #' O2_em_corr, pH_em_corr, O2_mmHg, pH and pH_em_corr_corr
 #'
 #' @noRd
 #' @keywords internal
 #'
-calc_background <- function(xf_raw_pr){
+calc_background <- function(xf_raw_pr) {
+    background <- xf_raw_pr %>%
+        filter(flagged_well == FALSE) %>%
+        dplyr::select(
+            group, well, tick, O2_em_corr,
+            pH_em_corr, O2_mmHg, pH, pH_em_corr_corr
+        ) %>%
+        dplyr::filter(group == "Background") %>%
+        dplyr::summarize(
+            O2_em_corr_bkg = mean(O2_em_corr),
+            pH_em_corr_bkg = mean(pH_em_corr),
+            O2_mmHg_bkg = mean(O2_mmHg),
+            pH_bkgd = mean(pH),
+            pH_em_corr_corr_bkg = mean(pH_em_corr_corr),
+            .by = c(tick)
+        )
 
-  background <- xf_raw_pr %>%
-    filter(flagged_well == FALSE) %>% 
-    dplyr::select(group, well, tick, O2_em_corr,
-           pH_em_corr, O2_mmHg, pH, pH_em_corr_corr) %>%
-    dplyr::filter(group == "Background") %>%
-    dplyr::summarize(
-      O2_em_corr_bkg = mean(O2_em_corr),
-      pH_em_corr_bkg = mean(pH_em_corr),
-      O2_mmHg_bkg = mean(O2_mmHg),
-      pH_bkgd = mean(pH),
-      pH_em_corr_corr_bkg = mean(pH_em_corr_corr),
-      .by = c(tick)
-    )
-
-  return(background)
+    return(background)
 }
 
 # END ---------------------------------------------------------------------
